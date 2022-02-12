a0969266d8e3662f89351a0c3e5c4a7c593257bc
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-12 22:55:55 +0600

    solve problem 1

---
+++ problem1.js
@@ -0,0 +1,11 @@
+// create "firstName" and "last_name" variables
+// assign your values
+// create "address" variable and assign "main street" value to it
+// re-assign address to "first street" later
+// log all values in the console
+
+const firstName = "Rakibul";
+const last_name = "Islam";
+let address = "main street";
+address = "first street";
+console.log(firstName, last_name, address);
