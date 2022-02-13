92392b62f4defae55c1365350ca320912e6dd868
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-13 22:46:03 +0600

    display commit

--- JavascriptBasic150problems/01_basic/01_basic.js
+++ JavascriptBasic150problems/01_basic/01_basic.js
@@ -8,12 +8,11 @@ const dayList = [
   "Friday",
   "Saturday",
 ];
-// console.log(dayList);
 
 // showing current day
 const day = dayList[d.getDay()];
-console.log("Today is :", day);
-console.log("Current time is :  ");
+let currentDay = console.log("Today is :", day);
+// console.log(currentDay);
 
 // showing am or pm ,Hour, minuit,seconds
 const minutes = d.getMinutes();
@@ -23,19 +22,8 @@ const Hour = d.getHours();
 // set Am Pm logic
 const amPm = Hour >= 12 ? "PM" : "AM";
 // console.log(amPm);
-const time =
-  "Current time is : ",
-  Hour,
-  " ",
-  amPm,
-  " : ",
-  minutes,
-  " : ",
-  seconds
-
-const displayTime = (document.getElementsByClassName("getDayTime").innerHtml =
-  time);
-
-console.log(displayTime);
-
-// document.getElementById("getDayTime").style.display = time;
+console.log(`Current time is : ${Hour} ${amPm} : ${minutes} :  ${seconds}  `);
+document.getElementById("getDayTime").innerHTML = `Today is : ${day}`;
+document.getElementById(
+  "getDayTime"
+).innerHTML = `Current time is : ${Hour} ${amPm} : ${minutes} :  ${seconds}  `;
