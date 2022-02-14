08f9236cb2ef3499e852279795c9662c48abb6bb
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-14 23:11:24 +0600

    print current page

---
+++ JavascriptBasic150problems/02_basic/02_basic.js
@@ -0,0 +1,8 @@
+// 2. Write a JavaScript program to print the contents of the current window.  Go to the editor
+// Click me to see the solution
+// window.print();
+// console.log(" My I You ");
+function print_current_page() {
+  window.print();
+}
+print_current_page();
